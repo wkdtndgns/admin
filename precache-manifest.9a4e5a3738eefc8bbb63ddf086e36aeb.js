bdd6bdfc24b9ea93137a8041f0a9df701df2f5fc
wkdtndgns <wkdtndgns@naver.com>
2019-08-24 03:48:02 +0900

    Updates

---
+++ precache-manifest.9a4e5a3738eefc8bbb63ddf086e36aeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58ebe84176eb87ec18a4b7f795643e67",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "2c1c74bbf2171ccb673a",
+    "url": "/admin/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "04d9fc4e20b2a3f2fe3d",
+    "url": "/admin/static/js/2.98c5ea59.chunk.js"
+  },
+  {
+    "revision": "2c1c74bbf2171ccb673a",
+    "url": "/admin/static/js/main.c979806e.chunk.js"
+  },
+  {
+    "revision": "d54e41971ce1ea5da763",
+    "url": "/admin/static/js/runtime~main.c90087cc.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/admin/static/media/logo.ee7cd8ed.svg"
+  }
+]);
